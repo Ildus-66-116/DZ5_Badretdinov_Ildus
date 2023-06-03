9d4cd7a7fd0bc0ad6745c40d935486a9aef3b328
ILDUS <ildus6696@gmail.com>
2023-06-03 22:36:34 +0300

    Dop_zadacha Statistika reshena

--- Program.cs
+++ Program.cs
@@ -56,39 +56,115 @@
 
 // Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
 
-void InputArray(double[] array)
+// void InputArray(double[] array)
+// {
+//     int end = 100, begin = 1;
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+// }
+
+// double MaxElement (double[] array)
+// {
+//     double Max = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(Max < array[i]) Max = array[i];
+//     }
+//     return Max;
+// }
+
+
+// double MinElement (double[] array)
+// {
+//     double Min = 100;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if(Min > array[i]) Min = array[i];
+//     }
+//     return Min;
+// }
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// double[] array = new double[n];
+// InputArray(array);
+// double Max = MaxElement(array);
+// double Min = MinElement(array);
+// Console.WriteLine($"Массив: [{string.Join(", ", array)}] => {Max} - {Min} = {Max - Min}");
+
+// Дополнительная задача Статистика
+
+void InputArray(int[] array)
 {
-    int end = 100, begin = 1;
     for (int i = 0; i < array.Length; i++)
-        array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
+        array[i] = new Random().Next(1, 32);
 }
 
-double MaxElement (double[] array)
+
+int CountNeChetnye(int[] array)
 {
-    double Max = 0;
-    for (int i = 0; i < array.Length; i++)
+    int countNch = 0;
+    foreach (int element in array)
     {
-        if(Max < array[i]) Max = array[i];
+        if (element % 2 == 1)
+            countNch++;
     }
-    return Max;
+    return countNch;
 }
 
-
-double MinElement (double[] array)
+int CountChetnye(int[] array)
 {
-    double Min = 100;
-    for (int i = 0; i < array.Length; i++)
+    int countCh = 0;
+    foreach (int element in array)
     {
-        if(Min > array[i]) Min = array[i];
+        if (element % 2 == 0)
+            countCh++;
     }
-    return Min;
+    return countCh;
+}
+
+int [] InputNeChet(int[] array, int[] resultNech)
+{
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 1) 
+        {
+            resultNech[j] =  array[i];
+            j = j +1;
+        }
+    }
+    return resultNech;
+}
+
+int [] InputChet(int[] array, int[] resultCh)
+{
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) 
+        {
+            resultCh[j] =  array[i];
+            j = j +1;
+        }
+    }
+    return resultCh;
 }
 
 Console.Clear();
 Console.Write("Введите кол-во элементов массива: ");
 int n = int.Parse(Console.ReadLine()!);
-double[] array = new double[n];
+int[] array = new int[n];
 InputArray(array);
-double Max = MaxElement(array);
-double Min = MinElement(array);
-Console.WriteLine($"Массив: [{string.Join(", ", array)}] => {Max} - {Min} = {Max - Min}");
+Console.WriteLine($"Массив: [{string.Join(", ", array)}]");
+int oc3 = CountNeChetnye(array);
+int oc4 = CountChetnye(array);
+int[] resultNech = new int[oc3];
+int[] resultCh = new int[oc4];
+InputNeChet(array, resultNech);
+Console.WriteLine($"Даты когда получил 3: [{string.Join(", ", resultNech)}]");
+InputChet(array, resultCh);
+Console.WriteLine($"Даты когда получил 4: [{string.Join(", ", resultCh)}]");
+if (oc3 < oc4) Console.WriteLine("Yes");
+else Console.WriteLine("No");
