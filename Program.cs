e518c04506681433a38436f7a9d452a2d3665168
ILDUS <ildus6696@gmail.com>
2023-06-03 19:21:34 +0300

    Zadacha 36 reshena

--- Program.cs
+++ Program.cs
@@ -2,21 +2,46 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(100, 1000);
+// }
+
+// int CountChetnye(int[] array)
+// {
+//     int count = 0;
+//     foreach (int element in array)
+//     {
+//         if (element % 2 == 0)
+//             count++;
+//     }
+//     return count;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// InputArray(array);
+// Console.WriteLine($"Массив: [{string.Join(", ", array)}] - > {CountChetnye(array)}");
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
 void InputArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(100, 1000);
+        array[i] = new Random().Next(0, 100);
 }
 
-int CountChetnye(int[] array)
+int SummaNechetnyx(int[] array)
 {
-    int count = 0;
-    foreach (int element in array)
-    {
-        if (element % 2 == 0)
-            count++;
-    }
-    return count;
+    int summa = 0;
+    for (int i = 1; i < array.Length; i = i + 2) summa = summa + array[i];
+    return summa;
 }
 
 
@@ -25,4 +50,5 @@ Console.Write("Введите кол-во элементов массива: ");
 int n = int.Parse(Console.ReadLine()!);
 int[] array = new int[n];
 InputArray(array);
-Console.WriteLine($"Массив: [{string.Join(", ", array)}] - > {CountChetnye(array)}");
+int Summa = SummaNechetnyx(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}] - > {Summa}");
