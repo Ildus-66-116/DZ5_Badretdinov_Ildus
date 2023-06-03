06141a2d171236237ad51eb7d7a40fe7514a2e1d
ILDUS <ildus6696@gmail.com>
2023-06-03 19:52:24 +0300

    Zadacha 38 reshena

--- Program.cs
+++ Program.cs
@@ -31,24 +31,64 @@
 
 // Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
-void InputArray(int[] array)
+// void InputArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(0, 100);
+// }
+
+// int SummaNechetnyx(int[] array)
+// {
+//     int summa = 0;
+//     for (int i = 1; i < array.Length; i = i + 2) summa = summa + array[i];
+//     return summa;
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите кол-во элементов массива: ");
+// int n = int.Parse(Console.ReadLine()!);
+// int[] array = new int[n];
+// InputArray(array);
+// int Summa = SummaNechetnyx(array);
+// Console.WriteLine($"Массив: [{string.Join(", ", array)}] - > {Summa}");
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива
+
+void InputArray(double[] array)
 {
+    int end = 100, begin = 1;
     for (int i = 0; i < array.Length; i++)
-        array[i] = new Random().Next(0, 100);
+        array[i] = Math.Round(new Random().NextDouble() * (end - begin) + begin, 2);
 }
 
-int SummaNechetnyx(int[] array)
+double MaxElement (double[] array)
 {
-    int summa = 0;
-    for (int i = 1; i < array.Length; i = i + 2) summa = summa + array[i];
-    return summa;
+    double Max = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(Max < array[i]) Max = array[i];
+    }
+    return Max;
 }
 
+
+double MinElement (double[] array)
+{
+    double Min = 100;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(Min > array[i]) Min = array[i];
+    }
+    return Min;
+}
 
 Console.Clear();
 Console.Write("Введите кол-во элементов массива: ");
 int n = int.Parse(Console.ReadLine()!);
-int[] array = new int[n];
+double[] array = new double[n];
 InputArray(array);
-int Summa = SummaNechetnyx(array);
-Console.WriteLine($"Массив: [{string.Join(", ", array)}] - > {Summa}");
+double Max = MaxElement(array);
+double Min = MinElement(array);
+Console.WriteLine($"Массив: [{string.Join(", ", array)}] => {Max} - {Min} = {Max - Min}");
